834317e23046a9bb839a7974deb8c33d0b16816d
musaib88 <musaibbashir2022@gmail.com>
2023-07-02 12:54:20 +0530

    slider

--- src/components/smart/HomeBrowse/HomeBrowse.js
+++ src/components/smart/HomeBrowse/HomeBrowse.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./HomeBrowse.css"
 import HomeBanner from "../../../img/wedensday.webp";
-import MovieList from '../../Dumb/MovieList/MovieList';
+import MovieList from '../MovieList/MovieList';
+
 export default function HomeBrowse() {
+  
+  
   return (
     <div>
         <div id='banner-homebrowse'>
            <img id='image-banner-layout-brow' src={HomeBanner} alt="" />
         </div>
         <div className='data-arousal-movies-row'>
-          <MovieList />
+            <MovieList></MovieList>
         </div>
     </div>
   )
